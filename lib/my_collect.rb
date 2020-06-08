47269ee14cfef852d9ab94d740f1ac9e5e8ea137
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-08 22:29:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,9 @@ def my_collect(array) # put argument(s) here
   result = []
   s = 0
   while s < l
-    yield (array[s]) = array[s]
+    yield result = (array[s])
+      result = array[s]
+    end
     s += 1
   end 
   return array
